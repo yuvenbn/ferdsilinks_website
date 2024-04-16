0662dc23b18532c6be20075600c6f467408bb391
yuvenbn <129100746+yuvenbn@users.noreply.github.com>
2024-04-16 15:40:50 +0100

    Update settings.py

--- ferdsilinks_website/settings.py
+++ ferdsilinks_website/settings.py
@@ -148,7 +148,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 # STATIC_ROOT = os.path.join(CORE_DIR, 'staticfiles')
-STATIC_ROOT = /var/www/html/ferdsilinks_website/ferdsilinks_website/static'
+STATIC_ROOT = '/var/www/html/ferdsilinks_website/ferdsilinks_website/static'
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(CORE_DIR, 'media')
